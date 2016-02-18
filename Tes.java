ad057d7fca1f7f67411a9338ff05dbc33a187f5f
muntako <akhmadmuntako@gmail.com>
2016-02-18 17:49:22 +0700

    test update

---
+++ Tes.java
@@ -0,0 +1,35 @@
+package JBank;
+
+
+/**
+ * Write a description of class Tes here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tes
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Tes
+     */
+    public Tes()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
